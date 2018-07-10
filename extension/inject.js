d941cb909621f2b62107d5876a93bdb749246637
samusbradus <sambradbury@djhenry.co.uk>
2018-07-10 21:00:14 +0100

    small insignificant changes

--- extension/inject.js
+++ extension/inject.js
@@ -1,1 +1,13 @@
-console.log("inject running");
+console.log("/// inject.js running");
+
+function changewithbidding() {
+  console.log("bid container changed");
+}
+
+const $bid_container = $(".app-container-main-view_content")
+
+$(document).ready(function(){
+ console.log("/// document ready");
+
+ $bid_container.bind("DOMSubtreeModified", changewithbidding);
+})
